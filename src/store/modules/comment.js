a3c257cd8243a30a79aa21a94754b3d2a29245c8
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2020-11-18 10:13:27 +0100

    clenup console logs

--- src/store/modules/comment.js
+++ src/store/modules/comment.js
@@ -37,27 +37,16 @@ const getters = {
   getRoot: (state) => (commentId) => { return state.rootComments.find(el => el.id === commentId) },
   getChildren: (state) => (commentId) => { return state.commentMap.get(commentId) },
   getCommentsByCodeLine: (state) => (courseDataId) => { // courseDataId
-    console.log('comment')
-    console.log(courseDataId)
-    console.log(state.dataLineSelection)
     const result = { roots: [], commentMap: state.commentMap }
     if (state.rootComments === null) { return result }
     if (state.dataLineSelection === null) { return result }
     result.roots = state.rootComments.filter(el => {
-      // el.linesFrom >= state.dataLineSelection && el.linesTo <= state.dataLineSelection)
       const goodcourseDataId = el.courseDataId === courseDataId
       const goodlinesFrom = state.dataLineSelection >= el.linesFrom
       const goodlinesTo = state.dataLineSelection <= el.linesTo
 
-      console.log('truths')
-      console.log(goodcourseDataId)
-      console.log(goodlinesFrom)
-      console.log(goodlinesTo)
-      console.log('end truths')
-
       return goodcourseDataId && goodlinesFrom && goodlinesTo
     })
-    console.log(result)
     return result
   },
   commentIsLoaded (state) { return state.rootComments !== null }
